f72a1aabe2e78cb048527c38579ae0845980efd0
Victor <vitor_ezeca@hotmail.com>
2020-05-24 14:50:02 -0300

    rota e favicon

--- batalha-naval/src/app/app-routing.module.ts
+++ batalha-naval/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from "./components/home/home.component";
+import { JogoComponent } from "./components/jogo/jogo.component";
+import { Erro404Component } from "./components/erro404/erro404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    data: {title: 'Batalha Naval - Início'}
+  },
+
+  {
+    path: "jogo",
+    component: JogoComponent,
+    data: {title: 'Batalha Naval - Jogo'}
+  },
+  {
+    path: '**', 
+    component: Erro404Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- batalha-naval/src/app/app.module.ts
+++ batalha-naval/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
+import { HomeComponent } from './components/home/home.component';
+import { JogoComponent } from './components/jogo/jogo.component';
+import { Erro404Component } from './components/erro404/erro404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JogoComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
